c600f5c2a28309532c4dcb75d0fcfe1324962986
Hung Huynh <hunghuynh@Hungs-MacBook-Pro.local>
2023-05-10 09:22:29 -0700

    testing openai

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
-import * as dotenv from "dotenv";
-import { OpenAI } from "langchain";
+import {ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate,} from "langchain/prompts";
+import {ChatOpenAI} from "langchain/chat_models/openai";
 
-dotenv.config();
-
-const model = new OpenAI({
-  modelName: "gpt-3.5-turbo",
-  openAIApiKey: process.env.OPENAI_API_KEY,
+const chat = new ChatOpenAI({
+    temperature: 0.9
 });
 
-const res = await model.call(
-  "What's a good idea for an application to build with GPT-3?"
-);
+const translationPrompt = ChatPromptTemplate.fromPromptMessages([
+    SystemMessagePromptTemplate.fromTemplate(
+        "You are a cheerful 3 legged kitty who loves petting and hopping around the deck.\n" +
+        "Your name is Ms Fiat. You have 3 sisters, Sweet Brown, Jeanne, and Little Ace. They are all " +
+        "wonder full well behaved cats. You love your daddies very much and demand their attention all the time." +
+        "Every night you would cuddle with them and fall asleep."
+    ),
+    HumanMessagePromptTemplate.fromTemplate("Why are you rubbing your tail on daddy?"),
+]);
 
-console.log(res);
+const responseA = await chat.generatePrompt([
+    await translationPrompt.formatPromptValue({
+        input_language: "English",
+        output_language: "Cat",
+        text: "I love beautiful men.",
+    }),
+]);
+
+console.log(JSON.stringify(responseA, null, 2));
